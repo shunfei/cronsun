50e34eeff7b9bf01c994b54ecbb8bdad2d4bc6de
QLeelulu <qleelulu@gmail.com>
2017-07-05 10:32:20 +0800

    better error log

--- common.go
+++ common.go
@@ -21,23 +21,23 @@ func Init() (err error) {
 
 	// init id creator
 	if err = initID(); err != nil {
-		return fmt.Errorf("Init UUID Generator faild: %s", err)
+		return fmt.Errorf("Init UUID Generator failed: %s", err)
 	}
 
 	// init config
 	if err = conf.Init(); err != nil {
-		return fmt.Errorf("Init Config faild: %s", err)
+		return fmt.Errorf("Init Config failed: %s", err)
 	}
 
 	// init etcd client
 	if DefalutClient, err = NewClient(conf.Config); err != nil {
-		return fmt.Errorf("Connect to ETCD %s faild: %s",
+		return fmt.Errorf("Connect to ETCD %s failed: %s",
 			conf.Config.Etcd.Endpoints, err)
 	}
 
 	// init mongoDB
 	if mgoDB, err = db.NewMdb(conf.Config.Mgo); err != nil {
-		return fmt.Errorf("Connect to MongoDB %s faild: %s",
+		return fmt.Errorf("Connect to MongoDB %s failed: %s",
 			conf.Config.Mgo.Hosts, err)
 	}
 
