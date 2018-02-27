870fb4a88cf34c9b95c90c1bd5c6c372bf24b80b
Doflatango <hst0012010@gmail.com>
2018-02-27 15:19:41 +0800

    Fix etcd configuration json fields

--- conf/conf.go
+++ conf/conf.go
@@ -8,6 +8,7 @@ import (
 	client "github.com/coreos/etcd/clientv3"
 	"github.com/fsnotify/fsnotify"
 	"github.com/go-gomail/gomail"
+	"github.com/k0kubun/pp"
 
 	"github.com/shunfei/cronsun/db"
 	"github.com/shunfei/cronsun/event"
@@ -63,12 +64,25 @@ type Conf struct {
 	// 默认 300
 	LockTtl int64
 
-	Etcd client.Config
+	Etcd *etcdConfig
 	Mgo  *db.Config
 	Web  *webConfig
 	Mail *MailConf
 
 	Security *Security
+}
+
+type etcdConfig struct {
+	Endpoints   []string
+	Username    string
+	Password    string
+	DialTimeout int64 // 单位秒
+
+	conf client.Config
+}
+
+func (e *etcdConfig) Copy() client.Config {
+	return e.conf
 }
 
 type webConfig struct {
@@ -131,8 +145,13 @@ func (c *Conf) parse() error {
 	}
 
 	if c.Etcd.DialTimeout > 0 {
-		c.Etcd.DialTimeout *= time.Second
+		c.Etcd.conf.DialTimeout = time.Duration(c.Etcd.DialTimeout) * time.Second
 	}
+	c.Etcd.conf.Username = c.Etcd.Username
+	c.Etcd.conf.Password = c.Etcd.Password
+	c.Etcd.conf.Endpoints = c.Etcd.Endpoints
+	pp.Println(c.Etcd.conf)
+
 	if c.Ttl <= 0 {
 		c.Ttl = 10
 	}
